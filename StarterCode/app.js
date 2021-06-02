b92f579e95243880fae76ed3bc7ad59a56c7ef63
Cecilia Aguayo <78163790+CAguayoRb@users.noreply.github.com>
2021-06-01 23:57:42 -0700

    almostdone

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,5 +1,5 @@
-var svgHeight = 900; //window.innerHeight;
-var svgWidth = 900;  //window.innerWidth;
+var svgHeight = 600; //window.innerHeight;
+var svgWidth = 960;  //window.innerWidth;
 
 var margin = {
     top: 20,
@@ -13,8 +13,8 @@ var width = svgWidth - margin.left - margin.right;
 
 var svg = d3.select("#scatter")
     .append("svg")
-    .attr("height", height)
-    .attr("width", width);
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
 
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
@@ -25,33 +25,24 @@ d3.csv("./assets/data/data.csv").then(function(scatterData){
     scatterData.forEach(function(data) {
         data.poverty = +data.poverty;
         data.healthcare = +data.healthcare;
-        data.age = +data.age;
-        data.obesity = +data.obesity;
+        //data.age = +data.age;
+        //data.obesity = +data.obesity;
         //console.log(data);
     });
 
     //Scale functions:
     var xLinearScale = d3.scaleLinear()
-        //.domain([0, d3.max(scatterData, d => d.poverty)])
+        .domain([0, d3.max(scatterData, d => d.poverty)])
         .range([0, width]);
     
     var yLinearScale = d3.scaleLinear()
-        //.domain([0, d3.max(scatterData, d => d.healthcare)])
+        .domain([0, d3.max(scatterData, d => d.healthcare)])
         .range([height, 0]);
     
     //Create axis functions:    
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
-    //Max and min 
-    var xAxisMin = d3.min(scatterData, d => d.healthcare);
-    var xAxisMax = d3.max(scatterData, d => d.healthcare);
-    var yAxisMin = d3.min(scatterData, d => d.poverty);
-    var yAxisMax = d3.max(scatterData, d => d.poverty);
-
-    //Set domains
-    xLinearScale.domain([xAxisMin, xAxisMax]);
-    yLinearScale.domain([yAxisMin, yAxisMax]);
 
     //Apend axes to the chart:
     chartGroup.append("g")
@@ -68,31 +59,34 @@ d3.csv("./assets/data/data.csv").then(function(scatterData){
     .append("circle")
     .attr("cx", d => xLinearScale(d.poverty))
     .attr("cy", d => yLinearScale(d.healthcare))
-    .attr("r", "10")
+    .attr("r", "15")
     .attr("fill", "red")
-    //.classed("stateCircle", true)
-    .attr("opacity", ".5");
+    .attr("opacity", ".4");
 
-    chartGroup.append("g")
+    chartGroup //.append("g")
     .selectAll("text")
     .data(scatterData)
     .enter()
-    .append("text")
-    .text(d => d.abbr)
+    .append('text')
     .attr("y", d => yLinearScale(d.healthcare))
     .attr("x", d => xLinearScale(d.healthcare))
-    .classed(".stateText", true)
+    .text(d => d.abbr)
     .attr("alignment-baseline", "central")
+    .attr("text-anchor", "middle")
+    .classed(".stateText", true)
 
     chartGroup.append("text")
-    .attr("transform", `translate(${-45+(width / 2)}, ${height + margin.top + 30})`)
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 20})`)
+    .attr("text-anchor", "middle")
     .attr("class", "axisText")
-    .attr("y", 0 - margin.left + 40)
-    .attr("x", -90 - (height / 2))
+    //.attr("y", 0 - margin.left + 40)
+    //.attr("x", -90 - (height / 2))
     .text("In Poverty (%)");
 
     chartGroup.append("text")
     .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", -90 - (height / 2))
     .attr("class", "axisText")
     .attr("alignment-baseline", "central")
     .text("Lacks Healthcare (%)");
